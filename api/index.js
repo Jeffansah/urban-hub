e21b96e436f8a0f25649bc2cea090812ed789890
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-01 18:48:55 +0000

    API routes created

---
+++ api/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+
+//instances
+dotenv.config();
+const app = express();
+const port = 3000;
+
+//mongoose connection
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongodb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from Mongodb");
+});
+
+// MIDDLEWARES //
+
+//parse json requests
+app.use(express.json());
+
+//routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/rooms", roomsRoute);
+app.use("/api/hotels", hotelsRoute);
+
+app.listen(port, () => {
+  connectToDatabase();
+  console.log(`listening on port: ${port}`);
+});
