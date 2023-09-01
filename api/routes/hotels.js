f98e285e18cbd52a414ef9c8918623eb4924990f
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-01 19:46:30 +0000

    Update hotels.js

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -3,6 +3,7 @@ import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
+//create hotel
 router.post("/", async (req, res) => {
   const newHotel = new Hotel(req.body);
 
@@ -13,6 +14,60 @@ router.post("/", async (req, res) => {
       .json({ message: "Successfully added a new hotel!", newHotel });
   } catch (error) {
     res.status(500).json({ error });
+  }
+});
+
+//update hotels
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    res
+      .status(200)
+      .json({ message: "Successfully updated hotel!", updatedHotel });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+//find all hotels
+router.get("/", async (req, res) => {
+  try {
+    const Hotels = await Hotel.find();
+
+    return res.status(200).json({ message: "Found Hotels!", Hotels });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+//find one hotel
+router.get("/:id", async (req, res) => {
+  try {
+    const foundHotel = await Hotel.findById(req.params.id);
+    return res.status(200).json({ message: "Found hotel!", foundHotel });
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+});
+
+//delete hotel
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedHotel = await Hotel.findOneAndDelete(req.params.id);
+
+    res
+      .status(200)
+      .json({ message: "Succesfully deleted hotel", deletedHotel });
+  } catch (error) {
+    res.status(401).json({ errror: "Error deleting hotel" });
   }
 });
 
