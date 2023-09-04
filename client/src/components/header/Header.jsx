4eee34d01e45ff279e0084536af1c65e01e3ffda
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-04 16:44:08 +0000

    Property List structure implemented

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -15,7 +15,7 @@ import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { format } from "date-fns";
 
-const Header = () => {
+const Header = ({ type }) => {
   const [date, setDate] = useState([
     {
       startDate: new Date(),
@@ -25,10 +25,29 @@ const Header = () => {
   ]);
 
   const [openDate, setOpenDate] = useState(false);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+
+  const handleOption = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "i" ? options[name]++ : options[name]--,
+      };
+    });
+  };
 
   return (
     <header className="bg-main text-white flex justify-center relative">
-      <div className="w-full max-w-5xl mt-5 mx-0 mb-[100px]">
+      <div
+        className={`w-full max-w-5xl mt-5 mx-0 mb-[100px] ${
+          type === "list" && "mb-0"
+        }`}
+      >
         <div className="flex gap-10 mb-[50px]">
           <div className="flex items-center gap-2.5 active">
             <FontAwesomeIcon icon={faBed} />
@@ -51,56 +70,138 @@ const Header = () => {
             <span>Airport taxis</span>
           </div>
         </div>
-        <h1 className="text-4xl font-semibold">
-          Unlock Endless Rewards: Unleash the Genius!
-        </h1>
-        <p className="my-5 mx-0">
-          Get rewarded for your travels – unlock instant savings of 10% or more
-          with a free Lamabooking account
-        </p>
-        <button className="bg-[#0071c2] text-white font-medium p-2.5 cursor-pointer">
-          Sign in / Register
-        </button>
-        <div className=" bg-white border-[3px] border-[#febb02] flex items-center justify-around py-2.5 px-0 rounded-md absolute bottom-[-25px] w-full max-w-5xl">
-          <div className="flex items-center gap-2.5">
-            <FontAwesomeIcon icon={faBed} className="text-gray-500" />
-            <input
-              type="text"
-              placeholder="Where are you going?"
-              className="border-none outline-none text-gray-600 placeholder:text-gray-600 focus:placeholder:text-gray-400"
-            />
-          </div>
-          <div className="flex items-center gap-2.5">
-            <FontAwesomeIcon icon={faCalendar} className="text-gray-500" />
-            <span
-              onClick={() => setOpenDate(!openDate)}
-              className="text-gray-600 cursor-pointer"
-            >{`${format(date[0].startDate, "iii, MMM dd")} — ${format(
-              date[0].endDate,
-              "iii, MMM dd"
-            )}`}</span>
-            {openDate && (
-              <DateRange
-                editableDateInputs={true}
-                onChange={(item) => {
-                  setDate([item.selection]);
-                }}
-                moveRangeOnFirstSelection={false}
-                ranges={date}
-                className="absolute top-[50px]"
-              />
-            )}
-          </div>
-          <div className="flex items-center gap-2.5">
-            <FontAwesomeIcon icon={faPerson} className="text-gray-500" />
-            <span className="text-gray-600">2 adults 2 children 1 room</span>
-          </div>
-          <div className="flex items-center gap-2.5">
+        {type !== "list" && (
+          <>
+            <h1 className="text-4xl font-semibold">
+              Unlock Endless Rewards: Unleash the Genius!
+            </h1>
+            <p className="my-5 mx-0">
+              Get rewarded for your travels – unlock instant savings of 10% or
+              more with a free UrbanHub account
+            </p>
             <button className="bg-[#0071c2] text-white font-medium p-2.5 cursor-pointer">
-              Search
+              Sign in / Register
             </button>
-          </div>
-        </div>
+            <div className=" bg-white border-[3px] border-[#febb02] flex items-center justify-around py-2.5 px-0 rounded-md absolute bottom-[-25px] w-full max-w-5xl">
+              <div className="flex items-center gap-2.5">
+                <FontAwesomeIcon icon={faBed} className="text-gray-500" />
+                <input
+                  type="text"
+                  placeholder="Where are you going?"
+                  className="border-none outline-none text-gray-600 placeholder:text-gray-600 focus:placeholder:text-gray-400"
+                />
+              </div>
+              <div className="flex items-center gap-2.5">
+                <FontAwesomeIcon icon={faCalendar} className="text-gray-500" />
+                <span
+                  onClick={() => {
+                    setOpenDate(!openDate);
+                    setOpenOptions(false);
+                  }}
+                  className="text-gray-600 cursor-pointer"
+                >{`${format(date[0].startDate, "iii, MMM dd")} — ${format(
+                  date[0].endDate,
+                  "iii, MMM dd"
+                )}`}</span>
+                {openDate && (
+                  <DateRange
+                    editableDateInputs={true}
+                    onChange={(item) => {
+                      setDate([item.selection]);
+                    }}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}
+                    className="absolute top-[50px] z-10"
+                  />
+                )}
+              </div>
+              <div className="flex items-center gap-2.5">
+                <FontAwesomeIcon icon={faPerson} className="text-gray-500" />
+                <span
+                  onClick={() => {
+                    setOpenOptions(!openOptions);
+                    setOpenDate(false);
+                  }}
+                  className="text-gray-600 cursor-pointer"
+                >
+                  {`${options.adult} ${
+                    options.adult === 1 ? "adult" : "adults"
+                  } · ${options.children} ${
+                    options.children === 1 ? "child" : "children"
+                  } · ${options.room} ${options.room === 1 ? "room" : "rooms"}`}
+                </span>
+                {openOptions && (
+                  <div className="absolute top-[50px] text-gray-600 rounded-md shadow-md bg-white z-10">
+                    <div className="flex w-[200px] justify-between m-2.5">
+                      <span className="optionText">Adults</span>
+                      <div className="flex gap-2.5 items-center text-black text-[12px]">
+                        <button
+                          disabled={options.adult <= 1}
+                          className="w-[30px] h-[30px] border border-[#0071c2] text-[#0071c2] cursor-pointer disabled:cursor-not-allowed"
+                          onClick={() => handleOption("adult", "d")}
+                        >
+                          -
+                        </button>
+                        <span className="optionCounter">{options.adult}</span>
+                        <button
+                          className="w-[30px] h-[30px] border border-[#0071c2] text-[#0071c2] cursor-pointer"
+                          onClick={() => handleOption("adult", "i")}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex w-[200px] justify-between m-2.5">
+                      <span className="optionText">Children</span>
+                      <div className="flex gap-2.5 items-center text-black text-[12px]">
+                        <button
+                          disabled={options.children <= 0}
+                          className="w-[30px] h-[30px] border border-[#0071c2] text-[#0071c2] cursor-pointer disabled:cursor-not-allowed"
+                          onClick={() => handleOption("children", "d")}
+                        >
+                          -
+                        </button>
+                        <span className="optionCounter">
+                          {options.children}
+                        </span>
+                        <button
+                          className="w-[30px] h-[30px] border border-[#0071c2] text-[#0071c2] cursor-pointer"
+                          onClick={() => handleOption("children", "i")}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex w-[200px] justify-between m-2.5">
+                      <span className="optionText">Rooms</span>
+                      <div className="flex gap-2.5 items-center text-black text-[12px]">
+                        <button
+                          disabled={options.room <= 1}
+                          className="w-[30px] h-[30px] border border-[#0071c2] text-[#0071c2] cursor-pointer disabled:cursor-not-allowed"
+                          onClick={() => handleOption("room", "d")}
+                        >
+                          -
+                        </button>
+                        <span className="optionCounter">{options.room}</span>
+                        <button
+                          className="w-[30px] h-[30px] border border-[#0071c2] text-[#0071c2] cursor-pointer"
+                          onClick={() => handleOption("room", "i")}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="flex items-center gap-2.5">
+                <button className="bg-[#0071c2] text-white font-medium p-2.5 cursor-pointer">
+                  Search
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 const Home = () => {
@@ -7,6 +9,13 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="mt-[50px] flex flex-col items-center gap-[30px]">
+        <Featured />
+        <h1 className="w-[1024px] text-lg font-bold">
+          Browse property by type
+        </h1>
+        <PropertyList />
+      </div>
     </div>
   );
 };
