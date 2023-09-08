0bc443e07ba4be2d6c367d7ec2d43b8b229afc73
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-08 01:25:28 +0000

    Tablet responsiveness to hotel search list added

--- client/src/components/searchResult/SearchResult.jsx
+++ client/src/components/searchResult/SearchResult.jsx
@@ -12,22 +12,28 @@ const SearchList = () => {
 
 const SearchResult = () => {
   return (
-    <div className="border border-[lightgray] p-2.5 rounded-[5px] flex justify-between gap-5 mb-5">
+    <div className="border border-[lightgray] p-2.5 lg:rounded-[5px] flex justify-between gap-5 mb-5">
       <img
         src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
         alt="hotel"
-        className="w-[200px] h-auto object-cover rounded-sm"
+        className="max-lg:w-[150px] w-[200px] h-auto object-cover rounded-sm"
       />
-      <div className="flex w-[600px]">
-        <div className="flex flex-col gap-2.5 flex-grow">
-          <h1 className="text-[20px] text-[#0071c2]">
+      <div className="flex w-[600px] max-md:text-xs">
+        <div className="flex flex-col gap-1.5 lg:gap-2.5 flex-grow">
+          <h1 className="md:text-[20px] text-[#0071c2] max-lg:whitespace-nowrap max-md:text-sm">
             Tower Street Apartments
           </h1>
+          <div className="flex gap-2 md:hidden items-center">
+            <span className="bg-main text-white text-sm p-1 font-bold rounded-tl-md rounded-tr-md rounded-br-md max-md:text-xs cursor-pointer">
+              8.9
+            </span>
+            <span className="font-medium">Excellent</span>
+          </div>
           <span className="text-[12px]">500m from center</span>
           <span className="text-[12px] bg-[#008009] text-white max-w-max p-[3px] rounded-[5px] py-[2px]">
             Free airport taxi
           </span>
-          <span className="text-[12px] font-bold">
+          <span className="text-[12px] font-bold max-lg:hidden">
             Studio Apartment with Air conditioning
           </span>
           <span className="text-[12px] ">
@@ -36,23 +42,32 @@ const SearchResult = () => {
           <span className="text-[12px] font-bold text-[#008009]">
             Free cancellation
           </span>
-          <span className="text-[12px] text-[#008009]">
+          <span className="text-[12px] text-[#008009] max-md:hidden">
             You can cancel later, so lock in this great price today!
           </span>
+          <div className="text-right flex flex-col gap-[5px] md:hidden">
+            <span className="lg:text-2xl max-lg:text-xl">$100</span>
+            <span className="text-[12px] text-gray-500">
+              Includes taxes and fees
+            </span>
+            <button className="bg-[#0071c2] text-white font-bold py-2.5 px-[5px] rounded-[5px] max-md:hidden">
+              See availability
+            </button>
+          </div>
         </div>
-        <div className="w-[30%] flex flex-col justify-between">
-          <div className="sRating flex justify-between items-start">
+        <div className="lg:w-[30%] flex flex-col justify-between max-md:hidden">
+          <div className="flex justify-between items-start">
             <span className="font-medium">Excellent</span>
-            <span className="bg-main text-white text-sm p-1 font-bold rounded-tl-md rounded-tr-md rounded-br-md max-md:text-xs cursor-pointer">
+            <span className="bg-main text-white lg:text-sm p-1 font-bold rounded-tl-md rounded-tr-md rounded-br-md cursor-pointer">
               8.9
             </span>
           </div>
-          <div className="text-right flex flex-col gap-[5px]">
+          <div className="text-right flex flex-col lg:gap-[5px]">
             <span className="text-2xl">$100</span>
             <span className="text-[12px] text-gray-500">
               Includes taxes and fees
             </span>
-            <button className="bg-[#0071c2] text-white font-bold py-2.5 px-[5px] rounded-[5px]">
+            <button className="bg-[#0071c2] text-white max-lg:font-semibold font-bold max-lg:py-1.5 py-2.5 px-[5px] rounded-[5px] max-md:hidden">
               See availability
             </button>
           </div>
--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -5,6 +5,8 @@ import { useLocation } from "react-router-dom";
 import format from "date-fns/format";
 import { DateRange } from "react-date-range";
 import SearchResult from "../../components/searchResult/SearchResult";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const List = () => {
   const location = useLocation();
@@ -12,6 +14,9 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [destination, setDestination] = useState(location.state.destination);
   const [options, setOptions] = useState(location.state.options);
+  const [adult, setAdult] = useState(location.state.options.adult);
+  const [children, setChildren] = useState(location.state.options.children);
+  const [room, setRoom] = useState(location.state.options.room);
   const [openDateModal, setOpenDateModal] = useState(false);
   const [openOptionsModal, setOpenOptionsModal] = useState(false);
 
@@ -19,84 +24,136 @@ const List = () => {
     <>
       <Navbar />
       <Header type="list" />
-      <div className="flex justify-center mt-5">
-        <div className="w-full max-w-5xl flex gap-5">
-          <div className=" bg-background  p-2.5 rounded-[10px] sticky top-10 min-w-[250px] text-xs max-h-[460px]">
-            <h1 className="text-xl font-bold text-gray-600 mb-2.5">Search</h1>
-            <div className="flex flex-col gap-[5px] mb-2.5">
-              <label className="text-[12px]">Destination</label>
-              <input
-                type="text"
-                placeholder={destination}
-                className="h-[30px] p-[5px] text-xs placeholder:text-xs placeholder:text-black rounded-sm"
-              />
-            </div>
-            <div className="flex flex-col gap-[5px] mb-2.5">
-              <label>Check-in/out Date</label>
-              <span
-                onClick={() => setOpenDateModal(!openDateModal)}
-                className="h-[30px] p-[5px] px-[8px] bg-white flex items-center text-xs cursor-pointer rounded-sm"
-              >{`${format(date[0].startDate, "iii, MMM dd")}  to  ${format(
-                date[0].endDate,
-                "iii, MMM dd"
-              )}`}</span>
-              {openDateModal && (
-                <DateRange
-                  onChange={(item) => {
-                    setDate([item.selection]);
-                  }}
-                  minDate={new Date()}
-                />
-              )}
-            </div>
-            <label className="mb-2.5">Options</label>
-            <div className="flex flex-col gap-2.5 p-2.5">
-              <div className="flex justify-between mb-2.5 text-[#555] text-[12px]">
-                <span className="lsOptionText">
-                  Min price <small>per night</small>
-                </span>
-                <input type="text" className="w-[50px] rounded-sm p-1" />
-              </div>
-              <div className="flex justify-between mb-2.5 text-[#555] text-[12px]">
-                <span className="lsOptionText">
-                  Min price <small>per night</small>
-                </span>
-                <input type="text" className="w-[50px] rounded-sm p-1" />
-              </div>
-
-              <div className="flex justify-between mb-2.5 text-[#555] text-[12px]">
-                <span className="lsOptionText">Adults</span>
-                <input
-                  type="number"
-                  min={1}
-                  className="w-[50px] rounded-sm p-1"
-                  placeholder={options.adult}
-                />
-              </div>
-
-              <div className="flex justify-between mb-2.5 text-[#555] text-[12px]">
-                <span className="lsOptionText">Children</span>
-                <input
-                  type="number"
-                  min={0}
-                  className="w-[50px] rounded-sm p-1"
-                  placeholder={options.children}
-                />
-              </div>
-
-              <div className="flex justify-between mb-2.5 text-[#555] text-[12px]">
-                <span className="lsOptionText">Room</span>
-                <input
-                  type="number"
-                  min={1}
-                  className="w-[50px] rounded-sm p-1"
-                  placeholder={options.room}
-                />
-              </div>
-            </div>
-            <button className="p-2.5 bg-[#0071c2] text-white w-full font-medium rounded-sm">
+      <div className="flex max-lg:flex-col justify-center lg:mt-5 max-lg:overflow-hidden">
+        <div className="w-full max-w-5xl flex gap-5  max-lg:flex-col">
+          <div className=" bg-background  p-2.5 lg:rounded-[10px] lg:sticky top-10 w-1/3 text-xs h-max lg:max-h-[720px] max-lg:w-full">
+            <h1 className="text-xl font-bold text-gray-600 mb-2.5 max-lg:hidden">
               Search
-            </button>
+            </h1>
+            <div className="max-lg:flex-col">
+              <div className="flex flex-col gap-[5px] mb-2.5  max-lg:gap-2">
+                <label className="text-[12px] max-lg:hidden">Destination</label>
+                <div className="max-lg:flex max-lg:bg-white max-lg:items-center max-lg:px-2 max-lg:gap-2">
+                  <FontAwesomeIcon
+                    icon={faSearch}
+                    className="lg:hidden h-4 w-4"
+                  />
+                  <input
+                    type="text"
+                    placeholder={destination}
+                    className="h-[30px] p-[5px] text-xs placeholder:text-xs placeholder:text-black rounded-sm w-full focus:outline-none"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col mb-2.5  max-lg:gap-2 max-lg:bg-white max-lg:flex-col max-lg:p-2">
+                <label className="lg:mb-[5px]">Check-in/out Date</label>
+                <span
+                  onClick={() => setOpenDateModal(!openDateModal)}
+                  className="h-[30px] p-[5px] px-[8px] bg-white flex items-center text-xs cursor-pointer rounded-sm max-lg:w-full max-lg:font-semibold"
+                >{`${format(date[0].startDate, "iii, MMM dd")}  to  ${format(
+                  date[0].endDate,
+                  "iii, MMM dd"
+                )}`}</span>
+                {openDateModal && (
+                  <DateRange
+                    onChange={(item) => {
+                      setDate([item.selection]);
+                    }}
+                    minDate={new Date()}
+                  />
+                )}
+              </div>
+              <label className="mb-2.5 max-lg:hidden">Options</label>
+              <div className="flex flex-col gap-2.5 p-2.5">
+                <div className="max-lg:flex justify-evenly">
+                  <div className=" max-lg:bg-white flex max-lg:flex-col lg:justify-between mb-2.5 text-[#555] text-[12px] max-lg:max-w-max max-lg:p-1 max-lg:w-1/2 max-lg:rounded-sm">
+                    <span className="">
+                      Min price <small>per night</small>
+                    </span>
+                    <input
+                      type="text"
+                      className="w-[50px] rounded-sm p-1 max-lg:w-full focus:outline-none"
+                    />
+                  </div>
+                  <div className="max-lg:bg-white flex max-lg:flex-col lg:justify-between max-lg:mb-2.5 text-[#555] text-[12px] max-lg:max-w-max max-lg:p-1 max-lg:w-1/2">
+                    <span className="">
+                      Min price <small>per night</small>
+                    </span>
+                    <input
+                      type="text"
+                      className="w-[50px] rounded-sm p-1 max-lg:w-full focus:outline-none"
+                    />
+                  </div>
+                </div>
+                <div className="max-lg:flex max-lg:gap-4 max-lg:justify-center max-lg:rounded-sm">
+                  <div className="flex justify-between mb-2.5 text-[#555] text-[12px] max-lg:bg-white max-lg:rounded-sm max-lg:p-2 max-lg:flex-col max-lg:flex-1">
+                    <span className="">Adults</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="w-[50px] rounded-sm p-1 max-lg:hidden focus:outline-none"
+                      placeholder={options.adult}
+                    />
+                    <select
+                      name=""
+                      id=""
+                      value={adult}
+                      onChange={(e) => setAdult(e.target.value)}
+                      className=" focus:outline-none lg:hidden"
+                    >
+                      {Array.from({ length: 20 }).map((_, index) => (
+                        <option>{index + 1}</option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div className="flex justify-between mb-2.5 text-[#555] text-[12px] max-lg:bg-white max-lg:rounded-sm max-lg:p-2 max-lg:flex-col max-lg:flex-1">
+                    <span className="lsOptionText">Children</span>
+                    <input
+                      type="number"
+                      min={0}
+                      className="w-[50px] rounded-sm p-1 max-lg:hidden focus:outline-none"
+                      placeholder={options.children}
+                    />
+                    <select
+                      name=""
+                      id=""
+                      value={children}
+                      onChange={(e) => setChildren(e.target.value)}
+                      className=" focus:outline-none lg:hidden"
+                    >
+                      {Array.from({ length: 10 }).map((_, index) => (
+                        <option>{index + 1}</option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div className="flex justify-between mb-2.5 text-[#555] text-[12px] max-lg:bg-white max-lg:rounded-sm max-lg:p-2 max-lg:flex-col max-lg:flex-1">
+                    <span className="lsOptionText">Room</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="w-[50px] rounded-sm p-1 max-lg:hidden focus:outline-none"
+                      placeholder={options.room}
+                    />
+                    <select
+                      name=""
+                      id=""
+                      value={room}
+                      onChange={(e) => setRoom(e.target.value)}
+                      className="focus:outline-none lg:hidden"
+                    >
+                      {Array.from({ length: 10 }).map((_, index) => (
+                        <option>{index + 1}</option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <button className="p-2.5 bg-[#0071c2] text-white w-full font-medium rounded-sm">
+                Search
+              </button>
+            </div>
           </div>
           <div className="">
             <SearchResult />
